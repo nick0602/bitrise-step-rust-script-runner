db766bf730f01af7bc026ae0ff66a08f7d0a839e
Niccolò Forlini <nick@niccoloforlini.com>
2022-07-31 20:31:10 +0100

    First commit

---
+++ rust_script_example.rs
@@ -0,0 +1,36 @@
+#!/usr/bin/env rust-script
+
+//!
+//! ```cargo
+//! [dependencies]
+//! rand = "0.8.0"
+//! ```
+
+use rand::prelude::*;
+
+fn main() {
+    let x: i8 = rand::thread_rng().gen_range(0..100);
+    println!("A random number from 0 to 100: {}", x);
+    let y: i8 = rand::thread_rng().gen_range(0..100);
+    println!("Another random number from 0 to 100: {}", y);
+
+    println!(
+        "The subtraction of these two random numbers: {}",
+        subtract(x, y)
+    );
+}
+
+fn subtract(n1: i8, n2: i8) -> i8 {
+    n1 - n2
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::subtract;
+
+    #[test]
+    fn the_subtraction_works() {
+        let expected_result = -35;
+        assert_eq!(expected_result, subtract(-10, 25));
+    }
+}
---
+++ step.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+install_rust_script_latest() {
+    cargo install rust-script
+}
+
+install_rust_script_env_version() {
+    cargo install rust-script --version $RUST_SCRIPT_VERSION
+}
+
+source "$HOME/.cargo/env"
+
+# If both envs are set, exit as it's undefined behavior.
+if [ "$RUST_SCRIPT_VERSION" ] && [ "$RUST_SCRIPT_AUTO_UPDATE" = true ]; then 
+    printf "Cannot set both RUST_SCRIPT_VERSION and RUST_SCRIPT_AUTO_UPDATE variables! Exiting..." >&2
+    exit 1
+fi
+
+# If rust-script is (not present OR autoupdate is enabled) AND (no custom version is set), then run cargo install with no version.
+if (! command -v rust-script &> /dev/null || [ "$RUST_SCRIPT_AUTO_UPDATE" = true ]) && [ -z "$RUST_SCRIPT_VERSION" ]; then
+    printf "Installing or updating rust-script..."
+    install_rust_script_latest
+elif [ "$RUST_SCRIPT_VERSION" ]; then # Otherwise install a specific version of rust-script.
+    printf "Selected rust-script version is $RUST_SCRIPT_VERSION, installing...\n"
+    install_rust_script_env_version
+fi
+
+rust-script $BITRISE_STEP_SOURCE_DIR/$RUST_SCRIPT_FILE_PATH
+
+case "$OSTYPE" in
+  darwin*)  
+    envman add --key RUST_SCRIPT_CACHE_PATH --value '~/Library/Caches/rust-script'
+    ;; 
+  
+  linux*)   
+    envman add --key RUST_SCRIPT_CACHE_PATH --value '~/.cache/rust-script'
+    ;;
+
+  *)        
+    printf "Cache path not supported for OS: $OSTYPE, skipping...\n\n" 
+    exit 0
+    ;;
+esac
+
+exit $?
